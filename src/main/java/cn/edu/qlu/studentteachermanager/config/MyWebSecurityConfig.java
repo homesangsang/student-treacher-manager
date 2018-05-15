28e40670697509f28cf0988bb6121fd837a02cdb
homesangsang <homesangsang@qq.com>
2018-05-15 18:14:39 +0800

    test

--- src/main/java/cn/edu/qlu/studentteachermanager/config/MyWebSecurityConfig.java
+++ src/main/java/cn/edu/qlu/studentteachermanager/config/MyWebSecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -39,5 +40,17 @@ public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers(HttpMethod.GET,
+                        "/",
+                        "/*.html",
+                        "favicon.ico",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js");
     }
 }
